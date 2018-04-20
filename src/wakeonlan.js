762ad9cfedd34c0301d481bc6f2a6fdbb16af3b4
Arturo Guerra <arturoguerra360@gmail.com>
2018-04-20 18:26:54 -0500

    Fixed tings

--- src/wakeonlan.js
+++ src/wakeonlan.js
@@ -1,37 +1,62 @@
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
 const wol = require('node-wol');
+const http = require('http');
+const fs = require('fs');
+
+const port = process.env.PORT || 8080
+const host = process.env.HOST || '0.0.0.0'
+const socket = process.env.SOCKET || null
+const broadcast = process.env.BROADCAST || null
+const password = process.env.PASSWORD || null
+
 const app = new express();
-const path = require('path');
-const config = require('./config.json');
-app.use(bodyParser.urlencoded({extended: false }));
+const httpServer = http.createServer(app);
+
+app.set('socket', socket);
+app.set('port', port);
+app.set('host', host);
+
+//app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
-app.use(bodyParser.text());
+//app.use(bodyParser.text());
 
 
 function parseMac(mac) {
-    return mac.replace(/-/g, ":");
+    return mac.replace(/-/g, ":")
 }
 
 app.post('/post', (req, res, next) => {
-    console.log(req.body.password);
-    if (req.body.password !== config.password) {
-        res.status(403).send("Sorry invalid password");
+    console.log('Password: ' + req.body.password);
+    if (req.body.password !== password) {
+        res.status(403).send('Sorry invalid password');
         return;
     }
     let options = {};
-    options.address = config.broadcast;
+    options.address = broadcast;
     let mac = parseMac(req.body.mac);
     console.log(mac);
     wol.wake(mac, options, (error) => {
         if (error) {
-            res.status(500).send("Internal server error a trained team of niggers is working on it")
+            res.status(500).send('Internal server error a trained team of niggers is working on it')
         } else {
-            res.status(200).send("Your wake on lan request has been sent!")
+            res.send('Your wake on lan request has been sent!')
         }
-    });
-});
+    })
+})
 
-app.listen(8808, () => {console.log("Running on port 8808")});
+function startServer () {
+  if (socket) {
+    if (fs.existsSync(socket)) {
+      fs.unlinkSync(socket)
+    }
+    httpServer.listen(socket, () => { console.log('Server listening on ' + socket) })
+    fs.chmodSync(socket, '0777')
+  } else {
+    httpServer.listen(port, host, () => {
+      console.log('Server listening on ' + host + ':' + port)
+    })
+  }
+}
 
-
+startServer()
